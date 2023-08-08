9c59c0f9c28a3a3e5b7d2d49d06ea1e2124b4bc0
Christian Dumas <christian.dumas@preaxis.com>
2023-08-07 21:51:11 -0400

    Add previously uncommitted code

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from flask import Flask
+
 from subprocess import check_output as co
 import time
 
@@ -10,9 +12,22 @@ def move_table(turn, tilt):
     co(['./controller.py ' + str(turn) + ' ' + str(tilt)], shell=True)
     co(['sudo systemctl stop pigpiod'], shell=True)
 
+app = Flask(__name__)
+
+@app.route("/move/<turn>&<tilt>")
+def move(turn, tilt):
+    move_table(turn, tilt)
+    return ""
+
+@app.route("/lights")
+def lights():
+    blink_leds(1)
+    return ""
+
+app.run(host='0.0.0.0', port=80)
 #blink_leds(1)
 #move_table(50, 100)
 
-for i in range(10):
-    time.sleep(1)
-    blink_leds(1)
+#for i in range(10):
+    #time.sleep(1)
+    #blink_leds(1)
